948ade7f755dea9560799da1354b97aee3616606
Epic312 <kaden_ostendorf@yahoo.com>
2021-02-21 17:39:18 -0700

    grape bunches (ignore the WIP cheese stuff lmao)

--- src/main/java/com/teamaurora/gourmandise/core/registry/GourmandiseItems.java
+++ src/main/java/com/teamaurora/gourmandise/core/registry/GourmandiseItems.java
@@ -16,11 +16,19 @@ public class GourmandiseItems {
 
     public static final RegistryObject<Item> GRAPE_BOAT = HELPER.createBoatItem("grape", GourmandiseBlocks.GRAPE_PLANKS);
 
+    public static final RegistryObject<Item> GRAPE_BUNCH = HELPER.createItem("grape_bunch", ()->new Item(new Item.Properties().group(ItemGroup.MISC))); // TODO: make it so this can be made into grapes
     public static final RegistryObject<Item> GRAPES = HELPER.createItem("grapes", ()->new Item(new Item.Properties().food(Foods.GRAPES).group(ItemGroup.FOOD)));
     public static final RegistryObject<Item> GRAPE_JUICE = HELPER.createItem("grape_juice", ()->new DrinkItem(new Item.Properties().containerItem(Items.GLASS_BOTTLE).food(Foods.GRAPE_JUICE).maxStackSize(16).group(ItemGroup.FOOD)));
+
+    public static final RegistryObject<Item> CHEESE = HELPER.createItem("cheese", ()->new Item(new Item.Properties().food(Foods.CHEESE).group(ItemGroup.FOOD)));
+    public static
 
     public static class Foods {
         public static final Food GRAPES = (new Food.Builder()).hunger(4).saturation(0.3F).build();
         public static final Food GRAPE_JUICE = (new Food.Builder()).hunger(2).saturation(0.1F).build();
+
+        public static final Food CHEESE = (new Food.Builder()).hunger(2).saturation(0.15F).build();
+        public static final Food CHEESE_SAUCE = (new Food.Builder()).hunger(3).saturation(0.2F).build();
+        public static final Food CHEESE_BREAD = (new Food.Builder()).hunger(7).saturation(0.7F).build();
     }
 }
