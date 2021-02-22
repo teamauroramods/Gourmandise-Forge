3abd961c49b1f2a69357892e66eb9db2092fa7dc
Epic312 <kaden_ostendorf@yahoo.com>
2021-02-21 17:47:05 -0700

    cheese (+sauce/bread)

--- src/main/java/com/teamaurora/gourmandise/core/registry/GourmandiseItems.java
+++ src/main/java/com/teamaurora/gourmandise/core/registry/GourmandiseItems.java
@@ -3,10 +3,7 @@ package com.teamaurora.gourmandise.core.registry;
 import com.minecraftabnormals.abnormals_core.core.util.registry.ItemSubRegistryHelper;
 import com.teamaurora.gourmandise.core.Gourmandise;
 import com.teamaurora.gourmandise.common.item.DrinkItem;
-import net.minecraft.item.Food;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 
@@ -21,7 +18,8 @@ public class GourmandiseItems {
     public static final RegistryObject<Item> GRAPE_JUICE = HELPER.createItem("grape_juice", ()->new DrinkItem(new Item.Properties().containerItem(Items.GLASS_BOTTLE).food(Foods.GRAPE_JUICE).maxStackSize(16).group(ItemGroup.FOOD)));
 
     public static final RegistryObject<Item> CHEESE = HELPER.createItem("cheese", ()->new Item(new Item.Properties().food(Foods.CHEESE).group(ItemGroup.FOOD)));
-    public static
+    public static final RegistryObject<Item> CHEESE_SAUCE = HELPER.createItem("cheese_sauce", ()->new SoupItem(new Item.Properties().food(Foods.CHEESE_SAUCE).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> CHEESE_BREAD = HELPER.createItem("cheese_bread", ()->new Item(new Item.Properties().food(Foods.CHEESE_BREAD).group(ItemGroup.FOOD)));
 
     public static class Foods {
         public static final Food GRAPES = (new Food.Builder()).hunger(4).saturation(0.3F).build();
